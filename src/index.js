0667bd9c15c25079ce7d0a1b6d4f96bf044ffaca
Érick Santos <71834322+erarich@users.noreply.github.com>
2023-07-29 17:36:52 -0300

    refactor: delete the old src folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/main.scss'
-import Person from './scripts/Person'
-import ExampleReactComponent from './scripts/ExampleReactComponent'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const person1 = new Person("Brad")
-ReactDOM.render(<ExampleReactComponent />, document.querySelector("#render-react-example-here"))
